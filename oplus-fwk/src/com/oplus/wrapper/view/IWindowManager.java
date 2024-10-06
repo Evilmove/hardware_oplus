3c8ddfae48f257c5ded9a83fec684daa0c16fecb
yograjfire18 <yograjfire18@gmail.com>
2024-10-06 15:26:34 +0530

    Revert "oplus-fwk: Fix build for V"
    This reverts commit 67b224f77e34db4791e6d4304f967fd5c63f74a8.

--- oplus-fwk/src/com/oplus/wrapper/view/IWindowManager.java
+++ oplus-fwk/src/com/oplus/wrapper/view/IWindowManager.java
@@ -31,6 +31,7 @@ import android.view.IWindowSession;
 import android.view.IWindowSessionCallback;
 import android.view.InputChannel;
 import android.view.InsetsState;
+import android.view.TaskTransitionSpec;
 import android.view.WindowContentFrameStats;
 import android.view.displayhash.DisplayHash;
 import android.view.displayhash.VerifiedDisplayHash;
