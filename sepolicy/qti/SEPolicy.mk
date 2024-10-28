1bfb4fdc8454ab543336f3f311424a182a2e8034
Evilmove <evilmove4378@gmail.com>
2024-10-28 13:05:19 +0530

    adapt to superior

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -28,3 +28,4 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/public/common-um
 endif
+include device/superior/sepolicy/libperfmgr/sepolicy.mk
